be15b954218cae4d919d0f27503d1b2e62943d3f
Julian Bacon <julian@merge.dev>
2021-03-26 12:45:10 -0700

    country

--- lib/merge_hris_client/models/country_enum.rb
+++ lib/merge_hris_client/models/country_enum.rb
@@ -276,8 +276,6 @@ module MergeHRISClient
     # @param [String] The enum value in the form of the string
     # @return [String] The enum value
     def build_from_hash(value)
-      constantValues = CountryEnum.constants.select { |c| CountryEnum::const_get(c) == value }
-      raise "Invalid ENUM value #{value} for class #CountryEnum" if constantValues.empty?
       value
     end
   end
